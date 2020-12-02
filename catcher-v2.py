1baafb562a293323c9e424793e00d7ed6f4b5fc5
Robin East <robin.east@xense.co.uk>
2020-12-02 14:11:12 +0000

    feed with a batch containing real image

--- catcher-v2.py
+++ catcher-v2.py
@@ -3,6 +3,25 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 from tf_pose.networks import get_network
 import tensorflow as tf
 import numpy as np
+import cv2 
+
+def _get_scaled_img(npimg):
+  target_size=(432, 368)
+  get_base_scale = lambda s, w, h: max(target_size[0] / float(h), target_size[1] / float(w)) * s
+  img_h, img_w = npimg.shape[:2]
+
+  # resize
+  npimg = cv2.resize(npimg, target_size, interpolation=cv2.INTER_CUBIC)
+  return [npimg], [(0.0, 0.0, 1.0, 1.0)]
+
+def get_test_input():
+  buffer = np.random.uniform(size=[10,368,432,3])
+  img_path = '/Users/robineast/projects/tf-pose-estimation/images/p1.jpg'  
+  img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+  print(f"(get_test_input) img.shape={img.shape}")
+  print(f"(get_test_input) scaled img shape={_get_scaled_img(img)[0][0].shape}")  
+  buffer[0] = _get_scaled_img(img)[0][0]
+  return buffer
 
 def load_model():
   input_node = tf.placeholder(tf.float32, shape=(10, 368, 432, 3), name='image')
@@ -20,6 +39,8 @@ def load_model():
   training_op = optimizer.minimize(loss, var_list=train_vars)
   return training_op, input_node, y_node, logits, xentropy
 
+
+
 train_op, input_node, y, logits, output = load_model()
 print(output)
 
@@ -29,7 +50,7 @@ with tf.Session() as sess:
   sess.run(init)
   result1, result_logits = sess.run([output, logits], feed_dict={
                      y: np.ones([10]),
-                     input_node: np.random.uniform(size=[10,368,432,3])
+                     input_node: get_test_input()
                    }) 
   print("result: ", result1)
   print("logits: ", result_logits) 

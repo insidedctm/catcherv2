97280da58ee9c0e805d2a1c7afeedf061df2fc28
Robin East <robin.east@xense.co.uk>
2020-12-07 19:18:56 +0000

    DataFlow experiment 1

---
+++ data_batch.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from tensorpack.dataflow import DataFromGenerator, BatchData
+
+def my_data_loader():
+  # load data from somewhere with Python, and yield them
+  manifest = pd.read_csv("/Users/robineast/projects/catcher/labels.csv")
+  exploded_manifest = manifest.apply(lambda row: list(map(lambda i: (row['filename'], i, 0), range(row['nofall_frame_start'],row['nofall_frame_end']+1))), axis=1)
+  frame_and_labels = exploded_manifest.explode()
+
+  for item in frame_and_labels:
+    yield [(item[0], item[1]), item[2]]
+
+df = DataFromGenerator(my_data_loader)
+
+for datapoint in df:
+    print(datapoint[0], datapoint[1])

123f7bee584b1003771c45e744cc95ab18f0ac8c
Robin East <robin.east@xense.co.uk>
2020-12-09 14:11:16 +0000

    add configuration for labels path, s3 prefix and whether to randomly shuffle instances to DataFlow

--- data_batch.py
+++ data_batch.py
@@ -5,12 +5,15 @@ from common import get_s3_url
 import cv2
 
 class MyDataFlow(DataFlow):
-  def __init__(self):
+  def __init__(self, labels_path="../catcher/labels.csv", prefix="train/", shuffle=True):
     self.image_cache = {}
+    self.labels_path = labels_path
+    self.prefix = prefix
+    self.shuffle = shuffle
 
   def __iter__(self):
     # load data from somewhere with Python, and yield them
-    manifest = pd.read_csv("../catcher/labels.csv")
+    manifest = pd.read_csv(self.labels_path)
     # temporarily remove .MOV files from processing
     manifest = manifest[manifest['filename'].apply(lambda x: not x.endswith('.MOV'))]    
     self.prefetch(manifest)
@@ -22,7 +25,9 @@ class MyDataFlow(DataFlow):
       return expand
 
     exploded_manifest = manifest.apply(expand_row, axis=1)
-    frame_and_labels = exploded_manifest.explode().sample(frac=1)
+    frame_and_labels = exploded_manifest.explode()
+    if self.shuffle:
+      frame_and_labels = frame_and_labels.sample(frac=1)
 
     for item in frame_and_labels:
       cache_key = f"{item[0]}-{item[1]}"
@@ -36,9 +41,9 @@ class MyDataFlow(DataFlow):
       frame_nos = get_frame_nos(item)
       self.add_images(item['filename'], frame_nos)
 
-  def add_images(self, key, frame_nos, prefix='train/'):
+  def add_images(self, key, frame_nos):
     bucket = 'catcher-videos'
-    url = get_s3_url(bucket, f"{prefix}{key}")
+    url = get_s3_url(bucket, f"{self.prefix}{key}")
     cap = cv2.VideoCapture(url)
     frame_cnt = 0
     while True:
@@ -50,15 +55,20 @@ class MyDataFlow(DataFlow):
         self.image_cache[f"{key}-{frame_cnt}"] = scaled_frame
       frame_cnt = frame_cnt + 1
 
-def get_dataflow(batch_size=50):
-  df = MyDataFlow()
-  df = BatchData(df, batch_size=batch_size)
+def get_dataflow(labels_path, batch_size=50, prefix="train/", shuffle=True):
+  df = MyDataFlow(labels_path, prefix, shuffle)
+  df = BatchData(df, batch_size=batch_size, remainder=True)
   df.reset_state()
   return df
 
 if __name__ == '__main__':
 
-  df = get_dataflow()
+  df = get_dataflow("../catcher/labels.csv")
+
+  for datapoint in df:
+    print(datapoint[0], datapoint[1])
+
+  df = get_dataflow("labels_validation.csv", prefix="", shuffle=False)
 
   for datapoint in df:
     print(datapoint[0], datapoint[1])

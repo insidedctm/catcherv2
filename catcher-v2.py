fb4e672f426a7bcd5f8ee2d6c27dd749f6da032c
Robin East <robin.east@xense.co.uk>
2020-12-10 16:33:13 +0000

    add validation accuracy to tensorboard summaries

--- catcher-v2.py
+++ catcher-v2.py
@@ -6,7 +6,6 @@ import numpy as np
 import cv2 
 import aws as aws
 import boto3
-from data import get_validation_data
 from tensorflow.python.platform import gfile
 from data_batch import get_dataflow
 import argparse
@@ -47,7 +46,7 @@ def load_model():
   optimizer = tf.train.AdamOptimizer()
   training_op = optimizer.minimize(loss, var_list=train_vars)
 
-  loss_summary = tf.summary.scalar('Training loss', loss)
+  loss_summary = tf.summary.scalar('Training_loss', loss)
   file_writer = tf.summary.FileWriter(logdir, tf.get_default_graph())
 
   return training_op, tensor_image, y_node, logits, xentropy, tensor_output, loss, loss_summary, file_writer
@@ -65,18 +64,24 @@ def validate(tf_sess):
     preds = [ 0 if x > y else 1 for (x,y) in y_pred_logits[0]]
     all_preds.extend(preds)
    
-  print_summary(all_labels, all_preds)
+  summary_stats = get_summary_stats(all_labels, all_preds)
+  
+  return summary_stats
 
-def print_summary(labels, preds):
+def get_summary_stats(labels, preds):
   labels = np.array(labels)
   preds  = np.array(preds)
-  print(f'accuracy: {(len(preds) - np.sum(np.square(preds-labels)))/len(preds):0.2}')
-
+  return {'validation_accuracy': (len(preds) - np.sum(np.square(preds-labels)))/len(preds)}
+  
 
 if __name__ == '__main__':
   args = parse_args()
   train_op, input_node, y, logits, output, _, loss, loss_summary, file_writer = load_model()
   print(output)
+
+  # validation summary setup
+  valid_accuracy_placeholder = tf.placeholder(dtype=tf.float32)
+  valid_accuracy = tf.summary.scalar('validation_accuracy', valid_accuracy_placeholder)
   
   init = tf.global_variables_initializer()
   # see if we can push an input through the mode
@@ -93,5 +98,7 @@ if __name__ == '__main__':
           file_writer.add_summary(summary_str, batch_index)
         _, loss_val = sess.run([train_op, loss], feed_dict={y_node: labels, tensor_image: input})
         batch_index = batch_index + 1
-      validate(sess)  
-  
+      summary_stats = validate(sess)  
+      print(f'accuracy: {summary_stats["validation_accuracy"]:0.2}')
+      summary_str = valid_accuracy.eval(feed_dict={valid_accuracy_placeholder: summary_stats['validation_accuracy']})
+      file_writer.add_summary(summary_str, batch_index)

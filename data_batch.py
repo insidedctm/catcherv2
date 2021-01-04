dfd6d8d1987ce8a7521d97586c55f944ae2aa434
Robin East <robin.east@xense.co.uk>
2021-01-04 12:01:12 +0000

    add depth parameter to VideoClipsDataFlow

--- data_batch.py
+++ data_batch.py
@@ -82,7 +82,8 @@ class MyDataFlow(DataFlow):
       return f'{directory}/{filename}'
 
 class VideoClipsDataFlow(MyDataFlow):
-  def __init__(self, labels_path, prefix, shuffle):
+  def __init__(self, labels_path, prefix, shuffle, depth=3):
+    self.depth = depth
     super().__init__()
     
   def __iter__(self):
@@ -102,17 +103,12 @@ class VideoClipsDataFlow(MyDataFlow):
       frame_and_labels = frame_and_labels.sample(frac=1)
 
     for item in frame_and_labels:
-      cache_key = f"{item[0]}-{item[1]}"
-      cache_key_minus1 = f"{item[0]}-{item[1]-1}" 
-      cache_key_minus2 = f"{item[0]}-{item[1]-2}"
-      if cache_key in self.image_cache:
-        if cache_key_minus1 in self.image_cache:
-          if cache_key_minus2 in self.image_cache:
-            img = self.image_cache[cache_key]
-            img_minus1 = self.image_cache[cache_key_minus1]
-            img_minus2 = self.image_cache[cache_key_minus2]
-            img_concatenated = np.vstack([img[np.newaxis], img_minus1[np.newaxis], img_minus2[np.newaxis]]) 
-            yield [img_concatenated, item[2]]
+      cache_keys = [f"{item[0]}-{item[1]-ix}" for ix in range(self.depth)]
+      if set(cache_keys).issubset(self.image_cache.keys()):
+        # get list of imgs from image_cache, inserting extra dimension in preparation for vstack
+        imgs = [self.image_cache[key][np.newaxis] for key in cache_keys]
+        img_concatenated = np.vstack(imgs) 
+        yield [img_concatenated, item[2]]
 
 def get_dataflow(labels_path, batch_size=50, prefix="train/", shuffle=True):
   df = MyDataFlow(labels_path, prefix, shuffle)
@@ -120,22 +116,28 @@ def get_dataflow(labels_path, batch_size=50, prefix="train/", shuffle=True):
   df.reset_state()
   return df
 
-def get_video_clips_dataflow(labels_path, batch_size=50, prefix="train/", shuffle=True):
-  df = VideoClipsDataFlow(labels_path, prefix, shuffle)
+def get_video_clips_dataflow(labels_path, batch_size=50, prefix="train/", shuffle=True, depth=3):
+  df = VideoClipsDataFlow(labels_path, prefix, shuffle, depth=depth)
   df = BatchData(df, batch_size=batch_size, remainder=True)
   df.reset_state()
   return df
 
 if __name__ == '__main__':
-
-  train = get_video_clips_dataflow("../catcher/labels.csv")
+  train = get_video_clips_dataflow("../catcher/labels.csv", depth=6)
 
   for datapoint in train:
-    print(datapoint[0], datapoint[1])
+    print(datapoint[0].shape, datapoint[1])
+    import matplotlib.pyplot as plt
+    fig = plt.figure()
+    for i in range(0,6):
+      ax = fig.add_subplot(2, 3, 6-i)
+      plt.imshow(datapoint[0][0][i])
+            
+    plt.show()
 
   valid = get_video_clips_dataflow("labels_validation.csv", prefix="", shuffle=False)
 
   for datapoint in valid:
-    print(datapoint[0], datapoint[1])
+    print(datapoint[0].shape, datapoint[1])
 
 

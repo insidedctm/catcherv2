ee5935e741d2b3f22e9130f27c59d1bcb04c7e92
Robin East <robin.east@xense.co.uk>
2021-01-06 14:43:04 +0000

    rename files implementing Conv3D model and training

--- Conv3DModel.py
+++ Conv3DModel.py
@@ -1,41 +1,42 @@
 import tensorflow as tf
 from tensorflow.python.platform import gfile
 from datetime import datetime
+from NaiveTransferLearningModel import SummaryWriter
 
-class SummaryWriter:
-  @staticmethod
-  def get_file_writer():
-    # tensorboard log directory name setup
-    now = datetime.utcnow().strftime("%Y%m%d%H%M%S")
-    root_logdir = "tf_logs"
-    logdir = "{}/run-{}/".format(root_logdir, now)
-    file_writer = tf.summary.FileWriter(logdir, tf.get_default_graph())
-  
-    return file_writer
-
-class NaiveTransferLearningModel:
+class Conv3DModel:
   def __init__(self):
     self.tensor_image = None
     
     self.saver = None
-    self.MODEL_NAME = 'NaiveTransferLearner'
+    self.MODEL_NAME = 'CatcherConv3D'
 
   def load_model(self, sess, restore_path=None, is_training=True):
       if restore_path:
+        sub_graph_name = 'MyImage1'
         imported_graph_saver = tf.train.import_meta_graph(f'{restore_path}.meta')
         imported_graph_saver.restore(sess, restore_path)
         self.y_node = tf.get_default_graph().get_tensor_by_name('y:0')
-        self.tensor_image = tf.get_default_graph().get_tensor_by_name('image:0')
-        self.tensor_output = tf.get_default_graph().get_tensor_by_name('Openpose/concat_stage7:0')
+        self.tensor_image = tf.get_default_graph().get_tensor_by_name(f'{sub_graph_name}/image:0')
+        self.tensor_output = tf.get_default_graph().get_tensor_by_name(f'{sub_graph_name}/Openpose/concat_stage7:0')
         self.logits = tf.get_default_graph().get_tensor_by_name('logits/BiasAdd:0') 
         self.loss   = tf.get_default_graph().get_tensor_by_name('loss:0')
         print('model restored')          
       else:
-        self.create_model_with_frozen_weights(sess)
+        depth=3
+        self.input_images = []
+        self.openpose_outputs = []
+        for ix in range(depth):
+          sub_graph_name = f'MyImage{ix}'
+          self.create_model_with_frozen_weights(sess, sub_graph_name=sub_graph_name)
+          image = tf.get_default_graph().get_tensor_by_name(f'{sub_graph_name}/image:0')
+          self.input_images.append(image)
+          self.openpose_outputs.append(tf.get_default_graph().get_tensor_by_name(f'{sub_graph_name}/Openpose/concat_stage7:0'))
+        self.tensor_image = self.input_images[0]
+        self.tensor_output = self.openpose_outputs[0]
         self.y_node = tf.placeholder(tf.int32, shape=[None], name="y")
-        self.tensor_image = tf.get_default_graph().get_tensor_by_name('image:0')
-        self.tensor_output = tf.get_default_graph().get_tensor_by_name('Openpose/concat_stage7:0')
-        flat1 = tf.reshape(self.tensor_output, shape=[-1, 46*54*57])
+        hmap_paf_tensor = tf.concat([tf.expand_dims(out, 1) for out in self.openpose_outputs], 1)
+        conv3d1 = tf.layers.conv3d(hmap_paf_tensor, 32, 3)
+        flat1 = tf.reshape(conv3d1, shape=[-1, 44*52*32])
         fc1 = tf.layers.dense(flat1, 64, activation=tf.nn.relu, name="fc1")
         self.logits = tf.layers.dense(fc1, 2, name="logits")
         xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=self.logits, labels=self.y_node)
@@ -68,16 +69,21 @@ class NaiveTransferLearningModel:
       summary_tensor = tf.summary.image(summ_name, value, max_outputs=max_outputs)
     return summary_tensor
 
-  def create_model_with_frozen_weights(self, sess):
+  def create_model_with_frozen_weights(self, sess, sub_graph_name):
     GRAPH_PB_PATH = '../tf-pose-estimation/models/graph/mobilenet_thin/graph_opt.pb'
     with gfile.FastGFile(GRAPH_PB_PATH,'rb') as f:
       graph_def = tf.GraphDef()
       graph_def.ParseFromString(f.read())
       sess.graph.as_default()
-      tf.import_graph_def(graph_def, name='')   
+      tf.import_graph_def(graph_def, name=sub_graph_name)   
  
-  def save(self, sess, epoch, save_path='checkpoint/'):
-    save_path = self.saver.save(sess, f'{save_path}{self.MODEL_NAME}_{epoch}.ckpt')
+  def save(self, sess, epoch, save_path='checkpoint/', experiment_name=None):
+    if experiment_name:
+      checkpoint_path = f'{save_path}{self.MODEL_NAME}_{experiment_name}_{epoch}.ckpt' 
+    else:
+      checkpoint_path = f'{save_path}{self.MODEL_NAME}_{epoch}.ckpt'
+    print(f'saving checkpoint to {checkpoint_path}, epoch={epoch}, save_path={save_path}, experiment_name={experiment_name}')
+    save_path = self.saver.save(sess, checkpoint_path)
     print(f'checkpoint saved to {save_path}')
 
   def get_training_op(self):
--- train_conv3d.py
+++ train_conv3d.py
@@ -6,27 +6,31 @@ import numpy as np
 import cv2 
 import aws as aws
 import boto3
-from data_batch import get_dataflow
+from data_batch import get_video_clips_dataflow
 import argparse
-from NaiveTransferLearningModel import NaiveTransferLearningModel, SummaryWriter
+from Conv3DModel import Conv3DModel, SummaryWriter
 
 def parse_args():
   parser = argparse.ArgumentParser()
   parser.add_argument('--batch_size', default=200, help='', type=int)
   parser.add_argument('--num_epochs', default=1, help='', type=int)
   parser.add_argument('--restore_path', help='if specified should be a tf.Saver checkpoint file to be loaded')
+  parser.add_argument('--experiment_label', default=None, help='Identifying text to be added to checkpoint filename')
   return parser.parse_args()
 
 def validate(tf_sess):
-  df = get_dataflow("labels_validation.csv", prefix="", shuffle=False)
+  df = get_video_clips_dataflow("labels_validation.csv", prefix="", shuffle=False, depth=3)
   all_labels = []
   all_preds = []
   for input, labels in df:
     all_labels.extend(labels) 
+    feed_dict = {}
+    feed_dict[model.y_node] = labels
+    for ix in range(len(model.input_images)):
+      input_tensor = model.input_images[ix]
+      feed_dict[input_tensor] = input[:,ix,:,:,:]
     y_pred_logits = tf_sess.run([model.logits], 
-                feed_dict= {
-                  model.tensor_image: input
-                })
+                feed_dict= feed_dict)
     preds = [ 0 if x > y else 1 for (x,y) in y_pred_logits[0]]
     all_preds.extend(preds)
 
@@ -34,7 +38,7 @@ def validate(tf_sess):
     
     false_negative_input = input[cond]
     for op in [img_summ_op, heatmap_summ_op, paf_summ_op]:
-      summary_str = sess.run(op, feed_dict={model.tensor_image: false_negative_input})
+      summary_str = sess.run(op, feed_dict={model.tensor_image: false_negative_input[:,0,:,:,:]})
       file_writer.add_summary(summary_str)
    
   summary_stats = get_summary_stats(all_labels, all_preds)
@@ -70,7 +74,7 @@ if __name__ == '__main__':
   sess = tf.Session()
 
   # construct and load model, prepare for training
-  model = NaiveTransferLearningModel()
+  model = Conv3DModel()
   model.load_model(sess, args.restore_path)
 
   # setup summary
@@ -95,14 +99,21 @@ if __name__ == '__main__':
     sess.run(init)
 
   batch_index = 0
-  df = get_dataflow("../catcher/labels.csv", batch_size=args.batch_size)
+  df = get_video_clips_dataflow("../catcher/labels.csv", batch_size=args.batch_size, depth=3)
   for epoch in range(args.num_epochs):
  
     for input, labels in df: 
       if batch_index % 100 == 0:
-        summary_str = model.loss_summary.eval(session=sess, feed_dict={model.y_node: labels, model.tensor_image: input}) 
-        file_writer.add_summary(summary_str, batch_index)
-      _, loss_val = sess.run([model.training_op, model.loss], feed_dict={model.y_node: labels, model.tensor_image: input})
+        tensor_image = input[:,0,:,:,:]
+        #summary_str = model.loss_summary.eval(session=sess, feed_dict={model.y_node: labels, model.tensor_image: tensor_image}) 
+        #file_writer.add_summary(summary_str, batch_index)
+      # construct feed dict and pass into training step
+      feed_dict = {}
+      feed_dict[model.y_node] = labels
+      for ix in range(len(model.input_images)):
+        input_tensor = model.input_images[ix]
+        feed_dict[input_tensor] = input[:,ix,:,:,:]
+      _, loss_val = sess.run([model.training_op, model.loss], feed_dict=feed_dict)
       batch_index = batch_index + 1
     summary_stats = validate(sess)  
     print(f'accuracy: {summary_stats["validation_accuracy"]:0.2}')
@@ -112,6 +123,6 @@ if __name__ == '__main__':
     file_writer.add_summary(summary_str, batch_index)
 
     # save checkpoint every epoch
-    model.save(sess, epoch)
+    model.save(sess, epoch, experiment_name=args.experiment_label)
 
   file_writer.close()
